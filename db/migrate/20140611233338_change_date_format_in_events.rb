f6eb1d4bc2825f2d9e7e8dd3f453ac0d8fc67e8f
Peter W <techieshark@gmail.com>
2014-07-30 01:34:24 -0700

    rolling migrations together
    - in another commit we’ll update …create_events.rb with correct datetime field

--- db/migrate/20140611233338_change_date_format_in_events.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDateFormatInEvents < ActiveRecord::Migration
-  def change
-  	execute "ALTER TABLE events ALTER COLUMN \"EventDate\" TYPE timestamp USING to_timestamp(\"EventDate\", 'YYYY-MM-DD');"
-  end
-end
-
