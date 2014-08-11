9b88bc70526801cdedda9abb70a7ad2c5f4be431
tom buckley <buckley.tom@gmail.com>
2014-08-11 14:19:30 -0700

    cleaning up sort-by, mixing in contentious and local items, and changing header name to test

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -30,8 +30,9 @@ class AddressesController < ApplicationController
       @addr = @address.full_address
       @district_polygon = CouncilDistrict.getDistrict @lat, @lng 
       if @district_polygon and @district_polygon.id
-         @event_items = EventItem.where({EventItemId: @testing_issues})
-#        @event_items = EventItem.joins(:event).where('"events"."EventDate" > ?', 2.month.ago).where(council_district_id: @district_polygon.id)
+         @event_items1 = EventItem.where({EventItemId: @testing_issues})
+         @event_items2 = EventItem.joins(:event).where('"events"."EventDate" > ?', 2.month.ago).where(council_district_id: @district_polygon.id)
+         @event_items = @event_items1 + @event_items2
       else
         puts "ERROR: Whaaaaaat?! No district/id. You ran rake mesa_councils:load to populate the table right?"
       end
