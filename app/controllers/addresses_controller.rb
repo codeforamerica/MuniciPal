641de9ddb22acdc459f57aeb3eee8513428e02e7
tom buckley <buckley.tom@gmail.com>
2014-08-29 19:41:48 -0700

    sort by date

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -31,7 +31,7 @@ class AddressesController < ApplicationController
       @district_polygon = CouncilDistrict.getDistrict @lat, @lng 
       if @district_polygon and @district_polygon.id
          @event_items1 = EventItem.where({EventItemId: @testing_issues})
-         @event_items2 = EventItem.joins(:event).where('"events"."EventDate" > ?', 2.month.ago).where(council_district_id: @district_polygon.id)
+         @event_items2 = EventItem.joins(:event).where('"events"."EventDate" > ?', 2.month.ago).where(council_district_id: @district_polygon.id).order('"EventItemLastModified" DESC')
          @event_items = @event_items2 # + @event_items1
       else
         puts "ERROR: Whaaaaaat?! No district/id. You ran rake mesa_councils:load to populate the table right?"
