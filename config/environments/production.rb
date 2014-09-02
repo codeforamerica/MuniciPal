d70021e0c6509ed5fe83baed94236c4819328c8e
tom buckley <buckley.tom@gmail.com>
2014-09-01 23:07:07 -0700

    changes to production for assets/images on heroku--not deployed yet

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Zone::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
