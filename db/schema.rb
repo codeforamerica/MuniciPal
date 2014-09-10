457f8eadcaf4578d1540b6c0a076cc5a6e290f1a
Peter W <techieshark@gmail.com>
2014-09-10 11:30:18 -0700

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20140727091138) do
   enable_extension "postgis"
 
   create_table "attachments", force: true do |t|
+    t.integer  "source_id"
     t.integer  "matter_id"
     t.text     "guid"
     t.datetime "last_modified_utc"
@@ -32,29 +33,27 @@ ActiveRecord::Schema.define(version: 20140727091138) do
     t.datetime "updated_at"
   end
 
-  create_table "council_districts", id: false, force: true do |t|
+  create_table "council_districts", force: true do |t|
     t.string   "name"
     t.string   "twit_name"
     t.string   "twit_wdgt"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.spatial  "geom",       limit: {:srid=>0, :type=>"geometry"}
-    t.integer  "id",                                               null: false
   end
 
   create_table "event_items", force: true do |t|
+    t.integer  "source_id"
     t.integer  "council_district_id"
     t.integer  "event_id"
     t.integer  "matter_id"
-    t.integer  "source_id"
     t.string   "guid"
-    t.datetime "last_modified"
     t.datetime "last_modified_utc"
     t.string   "row_version"
     t.integer  "agenda_sequence"
     t.integer  "minutes_sequence"
     t.string   "agenda_number"
-    t.string   "video"
+    t.integer  "video"
     t.integer  "video_index"
     t.string   "version"
     t.string   "agenda_note"
@@ -65,7 +64,7 @@ ActiveRecord::Schema.define(version: 20140727091138) do
     t.integer  "passed_flag"
     t.string   "passed_flag_name"
     t.integer  "roll_call_flag"
-    t.string   "flag_extra"
+    t.integer  "flag_extra"
     t.text     "title"
     t.string   "tally"
     t.integer  "consent"
@@ -83,6 +82,7 @@ ActiveRecord::Schema.define(version: 20140727091138) do
   end
 
   create_table "events", force: true do |t|
+    t.integer  "source_id"
     t.string   "guid"
     t.string   "last_modified"
     t.string   "last_modified_utc"
@@ -93,13 +93,16 @@ ActiveRecord::Schema.define(version: 20140727091138) do
     t.string   "time"
     t.string   "video_status"
     t.integer  "agenda_status_id"
+    t.string   "agenda_status_name"
     t.integer  "minutes_status_id"
+    t.string   "minutes_status_name"
     t.string   "location"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "matters", force: true do |t|
+    t.integer  "source_id"
     t.integer  "guid"
     t.datetime "last_modified_utc"
     t.string   "row_version"
