925aa1e65323d7218feb4cce6721873c140d7353
tom buckley <buckley.tom@gmail.com>
2014-09-11 20:21:00 -0700

    merging from peters backend stuff

--- db/schema.rb
+++ db/schema.rb
@@ -154,37 +154,4 @@ ActiveRecord::Schema.define(version: 20140727091138) do
     t.datetime "updated_at"
   end
 
-  create_table "matters", force: true do |t|
-    t.integer  "matterid"
-    t.integer  "matterguid"
-    t.datetime "matterlastmodifiedutc"
-    t.string   "matterrowversion"
-    t.string   "matterfile"
-    t.string   "mattername"
-    t.text     "mattertitle"
-    t.integer  "mattertypeid"
-    t.string   "mattertypename"
-    t.integer  "matterstatusid"
-    t.string   "matterstatusname"
-    t.integer  "matterbodyid"
-    t.text     "matterbodyname"
-    t.datetime "matterintrodate"
-    t.datetime "matteragendadate"
-    t.datetime "matterpasseddate"
-    t.datetime "matterenactmentdate"
-    t.integer  "matterenactmentnumber"
-    t.string   "matterrequester"
-    t.text     "matternotes"
-    t.string   "matterversion"
-    t.text     "mattertext1"
-    t.text     "mattertext2"
-    t.text     "mattertext3"
-    t.text     "mattertext4"
-    t.text     "mattertext5"
-    t.datetime "matterdate1"
-    t.datetime "matterdate2"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
