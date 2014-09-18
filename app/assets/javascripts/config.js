3e73bd7925df093ed3cba2911925727f78829f4f
Peter W <techieshark@gmail.com>
2014-09-17 20:38:26 -0700

    centralize configuration in config.js

---
+++ app/assets/javascripts/config.js
@@ -0,0 +1,14 @@
+
+var config = {
+	map: {
+		prj:'codeforamerica.hmebo8ll', // Mapbox map id string
+		center_location: [33.4019, -111.78],
+		marker_location: [33.42, -111.835],
+		start_zoom: 12,
+		district_fill: 'white'
+	},
+	disqus: {
+		shortname: 'yerhere', // required: replace example with your forum shortname
+		base_url: 'http://yerhere.herokuapp.com/'
+	}
+}
