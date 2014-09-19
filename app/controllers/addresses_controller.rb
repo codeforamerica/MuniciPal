0fc6106aace3a8a082fd852a8545d907fc0a6674
Peter W <techieshark@gmail.com>
2014-09-19 14:20:59 -0700

    refactor: adds comments, makes more readable

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -8,7 +8,8 @@ class AddressesController < ApplicationController
     @in_district = false
     @lat = nil, @lng = nil, @address = nil
 
-    if params[:address] != nil and !params[:address].empty?
+    # address given; geocode to get lat/lon
+    if not params[:address].blank?
       @address = Geokit::Geocoders::MultiGeocoder.geocode params[:address]
       @in_district = CouncilDistrict.inDistrict? @address.lat, @address.lng
       @lat = @address.lat
@@ -16,7 +17,8 @@ class AddressesController < ApplicationController
       puts "LAT/LON from address: " + @lat.to_s + "/" + @lng.to_s
     end
 
-    if params[:lat] != nil  and params[:long] != nil and !params[:lat].empty? and !params[:long].empty?
+    # lat/lon given, reverse geocode to find address
+    if not params[:lat].blank? and not params[:long].blank?
       @address = Geokit::Geocoders::MultiGeocoder.reverse_geocode "#{params[:lat]}, #{params[:long]}"
       @in_district = CouncilDistrict.inDistrict? params[:lat], params[:long]
       @lat = params[:lat]
@@ -24,7 +26,7 @@ class AddressesController < ApplicationController
       puts "LAT/LON from params: " + @lat.to_s + "/" + @lon.to_s
     end
 
-    if @address != nil
+    if @address
       @addr = @address.full_address
       @district_polygon = CouncilDistrict.getDistrict @lat, @lng
       if @district_polygon and @district_polygon.id
@@ -34,7 +36,6 @@ class AddressesController < ApplicationController
 
         attachments = @event_items.map(&:attachments) #see http://ablogaboutcode.com/2012/01/04/the-ampersand-operator-in-ruby/
         events = @event_items.map(&:event).uniq #see http://ablogaboutcode.com/2012/01/04/the-ampersand-operator-in-ruby/
-
       else
         puts "ERROR: Whaaaaaat?! No district/id. You ran rake council_districts:load to populate the table right?"
       end
