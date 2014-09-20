5afd0ed32e7ca9da706adafe53e4b5eb8b2a2f94
tom buckley <buckley.tom@gmail.com>
2014-09-19 17:55:51 -0700

    cleaned up SQL--this means that now on our app expects only epsg:4326 input districts

--- app/models/council_district.rb
+++ app/models/council_district.rb
@@ -6,23 +6,20 @@ class CouncilDistrict < ActiveRecord::Base
   COORD_SYS_ZONE = 0;     # The coordinate system used in the actual data but somehow it was wiped to 0, so put it back into the same one to check
 
   def self.inDistrict? lat, long
-    # figure out if it is in a specific area in 
-    @spec_area = CouncilDistrict.where("ST_Contains(geom, ST_SetSRID(ST_Transform(ST_SetSRID(ST_MakePoint(?, ?), ?), ?), ?))",
+
+    # figure out if it is in a specific area in
+    @spec_area = CouncilDistrict.where("ST_Contains(geom, ST_SetSRID(ST_MakePoint(?, ?),4326))",
                                               long,
-                                              lat,
-                                              COORD_SYS_REF,
-                                              COORD_SYS_AREA,
-                                              COORD_SYS_ZONE)
+                                              lat)
 
     return @spec_area.exists?
   end
- 
+
   def self.getDistrict lat, long
     # figure out if it is in a specific area in historical district
-    @area_in_geojson = CouncilDistrict.find_by_sql("select id, name, twit_name, twit_wdgt, ST_AsGeoJSON(ST_Transform(ST_SetSRID(geom, #{COORD_SYS_AREA}), #{COORD_SYS_REF}))
-                                                        from council_districts 
-                                                        where ST_Contains(geom,
-                                                        ST_SetSRID(ST_Transform(ST_SetSRID(ST_MakePoint(#{long}, #{lat}),#{COORD_SYS_REF}), #{COORD_SYS_AREA}), #{COORD_SYS_ZONE}))")
+    @area_in_geojson = CouncilDistrict.find_by_sql("select id, name, twit_name, twit_wdgt, ST_AsGeoJSON(geom)
+                                                        from council_districts
+                                                        where ST_Contains(geom,ST_SetSRID(ST_MakePoint(#{long}, #{lat}),4326))")
     return @area_in_geojson.first
   end
 
