a351cec0fee5f17adcbf93e3d03c83a99035eefe
Peter W <techieshark@gmail.com>
2014-10-06 16:38:07 -0700

    add scopes to easily run routine queries
    - in irb console, run EventItem.current.with_matters.in_district(3) 
      for an example that chains all together
    - the example code included in event.rb may be useful later but is not needed yet, hence still commented out.

--- app/models/event_item.rb
+++ app/models/event_item.rb
@@ -5,4 +5,9 @@ class EventItem < ActiveRecord::Base
   belongs_to :matter
     validates :event, presence: true
   has_many :attachments, through: :matter, source: :matter_attachments
+
+  scope :current, ->{ joins(:event).where("events.date > ?", 2.weeks.ago) }
+  scope :with_matters, ->{ where("matter_id IS NOT NULL") }
+  scope :in_district, ->(district){ where("council_district_id = ?", district) }
+
 end
