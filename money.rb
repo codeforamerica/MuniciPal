5b183d424018918a233017b7f8a9dfe50378ba60
tom buckley <buckley.tom@gmail.com>
2014-10-19 11:29:48 -0700

    removed old test money/$ regex

--- money.rb
+++
@@ -1,8 +0,0 @@
-EventItem.where(EventItemMatterType: "Resolution").each {|item| 
-@money = item.EventItemTitle.match(/\d+[,.]\d+/i)
-if @money 
-	puts "MONEY:" + @money.to_s
-	puts item.EventItemTitle
-else
-end
-};0
