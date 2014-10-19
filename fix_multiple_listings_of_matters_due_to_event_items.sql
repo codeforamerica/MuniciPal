76351c1ba215e725a46c226d9409893e2e725607
tom buckley <buckley.tom@gmail.com>
2014-10-19 11:31:57 -0700

    removed old test sql

--- fix_multiple_listings_of_matters_due_to_event_items.sql
+++
@@ -1,5 +0,0 @@
-DROP SEQUENCE event_items_id_seq;
-CREATE TABLE tmptb AS SELECT DISTINCT ON ("EventItemMatterId") * FROM event_items;
-DROP TABLE event_items;
-CREATE TABLE event_items AS SELECT * FROM tmptb;
-CREATE SEQUENCE event_items_id_seq;
