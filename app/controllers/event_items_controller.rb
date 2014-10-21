e1e2dfff6c64c47d5de79109aef7daa6ecb1840b
tom buckley <buckley.tom@gmail.com>
2014-10-20 20:02:23 -0700

    removed unused controller actions

--- app/controllers/event_items_controller.rb
+++ app/controllers/event_items_controller.rb
@@ -24,56 +24,6 @@ class EventItemsController < ApplicationController
     EventItem.find(params[:id])
   end
 
-
-  # GET /event_items/new
-  # def new
-  #   @event_item = EventItem.new
-  # end
-
-  # GET /event_items/1/edit
-  # def edit
-  # end
-
-  # POST /event_items
-  # POST /event_items.json
-  def create
-    @event_item = EventItem.new(event_item_params)
-
-    respond_to do |format|
-      if @event_item.save
-        format.html { redirect_to @event_item, notice: 'Event item was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @event_item }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @event_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /event_items/1
-  # PATCH/PUT /event_items/1.json
-  def update
-    respond_to do |format|
-      if @event_item.update(event_item_params)
-        format.html { redirect_to @event_item, notice: 'Event item was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @event_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /event_items/1
-  # DELETE /event_items/1.json
-  def destroy
-    @event_item.destroy
-    respond_to do |format|
-      format.html { redirect_to event_items_url }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_event_item
