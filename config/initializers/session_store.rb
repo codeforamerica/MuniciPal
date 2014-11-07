913233b426afc4a098fe95f570e4bb6ee7cd2446
Peter W <techieshark@gmail.com>
2014-11-07 11:00:42 -0800

    zone -> municipal for cookie too

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Municipal::Application.config.session_store :cookie_store, key: '_zone_session'
+Municipal::Application.config.session_store :cookie_store, key: '_municipal_session'
