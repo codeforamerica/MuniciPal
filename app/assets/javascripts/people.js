cc639e9f803ee8776daba9d7e0a391b0c51ed67d
jcturner <joyce.turner@mesaaz.gov>
2015-01-13 10:02:33 -0700

    Create people.js
    In the create people.js the bio, fb and twitter accounts for district 6 was changed.

---
+++ app/assets/javascripts/people.js
@@ -0,0 +1,200 @@
+var people = [
+ {
+   "title": "Mayor",
+   "district": "n/a",
+   "name": {
+     "first": "John ",
+     "last": "Giles",
+     "fullname": "John Giles"
+   },
+   "bio": "John Giles was elected the 40th Mayor of Mesa, Arizona in August, 2014.   Giles earned degrees from Brigham Young University in Political Science and Arizona State University's Sandra Day O'Connor College of Law.  He has managed  his own law firm in downtown Mesa for the past 20 years.  In 1996, Giles was elected to the Mesa City Council and from 1998 to 2000,  he served as Vice Mayor.  Mayor Giles is committed to leading the way to build  upon Mesa's strengths in economic development, education, community livability,  smart growth and accessibility and participation in City government.",
+   "social": [
+     {
+       "platform": "facebook",
+       "name": "MesaMayorsOffice"
+     },
+     {
+       "platform": "twitter",
+       "name": "MesaMayorOffice"
+     }
+   ],
+   "phone": "480-644-2388",
+   "email": "mayor@Mesaaz.gov",
+   "website_url": "http://www.mesaaz.gov/mayor/",
+   "photo_url": "https://techieshark.github.io/council-info/img/John_Giles_sm.jpg"
+ },
+ {
+   "title": "City Councilmember",
+   "district": "1",
+   "name": {
+     "first": "Dave",
+     "last": "Richins",
+     "fullname": "Dave Richins"
+   },
+   "bio": "Dave Richins was elected as the Councilmember representing Mesa’s District 1 in June of 2008 and was re-elected to a second term in August of 2012. Richins has been active on both a state and national level during his time on the City Council, representing the city on the National League of Cities’ Community and Economic Development steering committee.  He also serves on the Advisory Board of Smart Growth America's Local Leaders Council and sits on the U.S. Environmental Protection Agency's Local Government Advisory Council and the Arizona Power Plant and Line Sitting Commission.  Councilmember Richins earned his Bachelor of Arts degree in Sustainable Community Development from Prescott College. ",
+   "social": [
+     {
+       "platform": "facebook",
+       "name": "CouncilmemberDaveRichins"
+     },
+     {
+       "platform": "twitter",
+       "name": "dlrichins",
+       "widget": "465935532589993984"
+     }
+   ],
+   "phone": "480-644-4002",
+   "email": "District1@mesaaz.gov",
+   "website_url": "http://www.mesaaz.gov/council/richins/default.aspx",
+   "photo_url": "https://techieshark.github.io/council-info/img/Dave_Richins_sm.jpg"
+ },
+ {
+   "title": "City Councilmember",
+   "district": "2",
+   "name": {
+     "first": "Alex",
+     "last": "Finter",
+     "fullname": "Alex Finter"
+   },
+   "bio": "Elected to the Mesa City Council in June of 2008 and re-elected in August of 2012, Alex Finter is in his second term as the Councilmember for District 2. Finter was chosen as Vice Mayor of the City Council in January 2013, holding that title until he became the Mayor of Mesa in April 2014.   He served as Mesa’s Mayor until September 18, 2014, when he resumed his role as the District 2 Councilmember. During his time in the Mayor’s office, Finter helped implement both the Falcon Field Strategic Visioning Commission and the Downtown Vision Committee, as well as the Hometown Heroes banner program. In the private-sector, Finter is a businessman and partner at Worldwide Investments LLC. ",
+   "social": [
+     {
+       "platform": "facebook",
+       "name": "CouncilmemberAlexFinter"
+     },
+     {
+       "platform": "twitter",
+       "name": "MesaDistrict2",
+       "widget": "465941398470922240"
+     }
+   ],
+   "phone": "480-644-3772",
+   "email": "councilmember.finter@Mesaaz.gov",
+   "website_url": "http://www.mesaaz.gov/council/finter/",
+   "photo_url": "https://techieshark.github.io/council-info/img/Alex_Finter_sm.jpg"
+ },
+ {
+   "title": "City Councilmember",
+   "district": "3",
+   "name": {
+     "first": "Dennis",
+     "last": "Kavanaugh ",
+     "fullname": "Dennis Kavanaugh "
+   },
+   "bio": "Dennis Kavanaugh was elected to serve as the Mesa City Councilmember for District 3 in June of 2008 and was re-elected in August of 2012. Kavanaugh also was a member of the City Council from 1996 until 2004, serving as Vice Mayor from 2002 to 2004. Kavanaugh sits on several different committees and boards, including serving as the chairperson of the Council Public Safety committee. He currently sits on the Mesa Community College Commission on Excellence in Education and is the Vice Chair of the Valley Metro Rail board.  Councilmember Kavanaugh received his J.D. degree in from Arizona State University and a B.A. in Political Science from St. Louis University.",
+   "social": [
+     {
+       "platform": "facebook",
+       "name": "councilmember.kavanaugh"
+     },
+     {
+       "platform": "twitter",
+       "name": "MesaDistrict3",
+       "widget": "465941592985968641"
+     }
+   ],
+   "phone": "480-644-3003",
+   "email": "District3@mesaaz.gov",
+   "website_url": "http://www.mesaaz.gov/council/kavanaugh/default.aspx",
+   "photo_url": "https://techieshark.github.io/council-info/img/Dennis_Kavanaugh_sm.jpg"
+ },
+ {
+   "title": "City Councilmember",
+   "district": "4",
+   "name": {
+     "first": "Chris",
+     "last": "Glover",
+     "fullname": "Chris Glover"
+   },
+   "bio": "Serving his first term representing Mesa's District 4, Vice Mayor Chris Glover became the youngest person ever to serve on the Mesa City Council following his election in 2011. Glover is a native of Mesa, attending Mesa Public Schools, graduating with honors from Mesa High School and he was a member of the Mayor's Youth Committee.  Vice Mayor Glover earned a degree in Political Science with minors in  History and Spanish and a Master of Science in Management from   Arizona State University.",
+   "social": [
+     {
+       "platform": "facebook",
+       "name": "CouncilmemberChrisGlover"
+     },
+     {
+       "platform": "twitter",
+       "name": "MesaDistrict4",
+       "widget": "465941739635613696"
+     }
+   ],
+   "phone": "480-644- 3004",
+   "email": "councilmember.glover@Mesaaz.gov",
+   "website_url": "http://www.mesaaz.gov/council/glover/",
+   "photo_url": "https://techieshark.github.io/council-info/img/Chris_Glover_sm.jpg"
+ },
+ {
+   "title": "City Councilmember",
+   "district": "5",
+   "name": {
+     "first": "David",
+     "last": "Luna",
+     "fullname": "David Luna"
+   },
+   "bio": "David Luna was appointed to fill the vacant District 5 seat on the Mesa City Council in September of 2013. Luna has been recognized for his community involvement with numerous awards, including the Mesa United Way Volunteer of the Year Award in 2006, the City of Mesa National Emergency Preparedness Recognition in 2007, the Valle del Sol Hall of Fame Award in 2011 and the Mesa United Way Spirit of Mesa Award in 2012 and the City of Mesa's Citizen of the Year in 2012.  Luna was also inducted into the Silver Circle Society (National Academy of Arts and Science) Rocky Mountain Chapter  Councilmember Luna earned his Bachelor of Arts in Radio/Television from the University of Arizona, a Master of Mass Communication from Arizona State University and his Doctorate in Educational Leadership from Northern Arizona University in 2012. ",
+   "social": [
+     {
+       "platform": "facebook",
+       "name": "CouncilmemberDavidLuna"
+     },
+     {
+       "platform": "twitter",
+       "name": "MesaDistrict5",
+       "widget": "465941843922792448"
+     }
+   ],
+   "phone": "480-644-3771",
+   "email": "councilmember.luna@Mesaaz.gov",
+   "website_url": "http://www.mesaaz.gov/council/luna/default.aspx",
+   "photo_url": "https://techieshark.github.io/council-info/img/David_Luna_sm.jpg"
+ },
+ {
+   "title": "City Councilmember",
+   "district": "6",
+   "name": {
+     "first": "Kevin",
+     "last": "Thompson",
+     "fullname": "Kevin Thompson"
+   },
+   "bio": "Elected to the Mesa City Council in August 2014, Councilmember Kevin Thompson began his first term representing District 6 in January of 2015. His term on the Council runs until January of 2019. Thompson is very active in the community, serving on the Board of Directors for the Mesa Chamber of Commerce and the Centers for Habilitation. He was a member of the Military and Veterans Affairs Committee at the Arizona Chamber of Commerce, is the past chair of the Government Affairs Committee for the American Public Works Association and the chair of the National Utility Contractors Association.  He is also the secretary of the Tonto Recreation Alliance and is a 2012 graduate of the Mesa Leadership Training and Development program. A veteran of the United States Air Force, Thompson spent eight years in the service, including serving in Operation Storm. Kevin and his wife Donna have two children.",
+   "social": [
+     {
+       "platform": "facebook",
+       "name": "CouncilmemberKevinThompson"
+     },
+     {
+       "platform": "twitter",
+       "name": "MesaDistrict6",
+       "widget": "465941964072828928"
+     }
+   ],
+   "phone": "480-644-4003",
+   "email": "councilmember.thompson@Mesaaz.gov",
+   "website_url": "http://www.mesaaz.gov/council/thompson/default.aspx",
+   "photo_url": "https://techieshark.github.io/council-info/img/Scott_Somers_sm.jpg"
+ },
+ {
+   "title": "City Manager",
+   "district": "n/a",
+   "name": {
+     "first": "Chris",
+     "last": "Brady",
+     "fullname": "Chris Brady"
+   },
+   "bio": "The City Manager, as the City's chief administrative officer, administers the functions of the City in accordance with the City Charter, Ordinances, and established policies.  The City Manager’s Office provides centralized direction and leadership for the effective administration and operations of all municipal services for the City of Mesa as directed by the City Council and serves as the focal point for the management of the City staff departments.  The City Manager’s Office prepares and submits to the City Council a balanced budget of municipal services in adherence with the policy goals and objectives by the City Council while employing such managerial techniques as needed to assure efficient and effective utilization of the City’s resources.",
+   "social": [
+     {
+       "platform": "facebook",
+       "name": "CityofMesa"
+     },
+     {
+       "platform": "twitter",
+       "name": "MesaCityManager"
+     }
+    ],
+   "phone": "480-644-2066",
+   "email": "COMmanager@Mesaaz.gov",
+   "website_url": "http://www.mesaaz.gov/mayor/",
+   "photo_url": "https://techieshark.github.io/council-info/img/Chris_Brady_sm.jpg"
+  },
+]
