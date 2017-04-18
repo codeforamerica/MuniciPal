f07233332dc734c2010fbd238afc7204f66789b6
Peter W <techieshark@gmail.com>
2017-04-17 23:34:46 -0700

    Add support for theming
    - this way, views can be overridden by themes (e.g. 'mesa' theme)
      without needing to change general / initial 'theme'. In other words,
      we can easily support multiple locations.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  if !ENV['THEME'].blank?
+    before_action :prepend_view_paths
+  end
+
+  def prepend_view_paths
+    # See .env.sample for info on setting & using a theme.
+    theme_dir = Rails.root + "app/views/themes/#{ENV['THEME']}"
+    # puts "prepending theme dir: #{theme_dir}"
+    prepend_view_path theme_dir
+  end
 end
